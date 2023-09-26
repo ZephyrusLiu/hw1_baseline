489eef3a8a015afc6677bba7b8f91d714c554bea
Yuqi Liu <yuqliu@umass.edu>
2023-09-26 14:37:50 -0400

    complete todo 3&4

--- expense_tracker/src/InputValidation.java
+++ expense_tracker/src/InputValidation.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.List; 
+
+
 public class InputValidation {
      
     public static void validateAmount(double amount) throws IllegalArgumentException {
@@ -13,6 +17,20 @@ public class InputValidation {
         // 2. It should be a valid number.
         if (!String.valueOf(amount).matches("^\\d+(\\.\\d+)?$")) {
             throw new IllegalArgumentException("Amount is not a valid number.");
+        }
+        
+    }
+
+    //3. The ```category``` should be a valid string input from the following list: "food", "travel", "bills", "entertainment", "other" .
+    private static String[] validCategories = {"food", "travel", "bills", "entertainment", "other"};
+    private static List<String> validCategoriesList = Arrays.asList(validCategories);
+
+    public static void validateCategory(String category) throws IllegalArgumentException {
+        // Incase input like "Food".
+        String inputCategory = category.toLowerCase();
+        // 4. You should display error messages on the GUI and not accept the invalid input. . You should throw an appropriate exception. 
+        if (!validCategoriesList.contains(inputCategory)) {
+            throw new IllegalArgumentException("Category is not valid. Valid categories are: food, travel, bills, entertainment, other.");
         }
     }
 }
